bdb1f1e9e0783fdc6dffd12c54bb57925efcf376
Dikshiya <dikshiyamahajan007@gmail.com>
2021-03-20 02:54:51 +0530

    updated code for pagination.

---
+++ app/src/main/java/com/example/postit/LoadMoreVH.java
@@ -0,0 +1,15 @@
+package com.example.postit;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LoadMoreVH extends RecyclerView.ViewHolder {
+        public static final int TYPE_LOAD_MORE = 100;
+
+        public LoadMoreVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
