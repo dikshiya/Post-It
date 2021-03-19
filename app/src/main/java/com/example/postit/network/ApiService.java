274998aa11d2e35b7a1510928bd821aa40d3d20d
Dikshiya <dikshiyamahajan007@gmail.com>
2021-03-20 02:46:42 +0530

    refactored MainActivity.java and updated ApiService.java

--- app/src/main/java/com/example/postit/network/ApiService.java
+++ app/src/main/java/com/example/postit/network/ApiService.java
@@ -1,20 +1,22 @@
 package com.example.postit.network;
 
-import com.example.postit.models.typeResults;
+import com.example.postit.models.commentResults;
+import com.example.postit.models.postResults;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
     @Headers("app-id: 60526989411adb7ee56da94e")
     @GET("post")
-    Call<typeResults> getPosts(@Query("app-id") String appId);
+    Call<postResults> getPosts(@Query("page") int page,
+                               @Query("limit") int limit);
 
-    @Headers("app-id : 60526989411adb7ee56da94e")
+    @Headers("app-id: 60526989411adb7ee56da94e")
     @GET("post/{postId}/comment")
-    Call<typeResults> getComments(@Query("app-id") String appId,
-                       @Query("postId") String postId);
+    Call<commentResults> getComments(@Path("postId") String postId);
 }
